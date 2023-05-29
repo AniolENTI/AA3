841474ceb21863fe563efd84caa5c225019d2d86
Aniol <aniol.morato@enti.cat>
2023-05-29 16:37:43 +0200

    Added Renderer Fixes from classlife

--- glframework-main/code/src/renderers/Renderer.cpp
+++ glframework-main/code/src/renderers/Renderer.cpp
@@ -3,7 +3,7 @@
 Renderer::Renderer(int width, int height)
 {
 	glViewport(0, 0, width, height);
-	glClearColor(bgColor[0],bgColor[1],bgColor[2],bgColor[3]);
+	glClearColor(bgColor[0], bgColor[1], bgColor[2], bgColor[3]);
 	glClearDepth(1.f);
 
 	cam._projection = glm::perspective(FOV, (float)width / (float)height, zNear, zFar);
@@ -90,10 +90,11 @@ void Renderer::GLrender(float dt)
 
 	cam._modelView = glm::mat4(1.f);
 	cam._modelView = glm::translate(cam._modelView, glm::vec3(panv[0], panv[1], panv[2]));
+	cam._modelView = glm::rotate(cam._modelView, rota[1], glm::vec3(1.f, 0.f, 0.f));
+	cam._modelView = glm::rotate(cam._modelView, rota[0], glm::vec3(0.f, 1.f, 0.f));
+
 	cam._cameraRotationMat = glm::rotate(glm::mat4(), rota[1], glm::vec3(1.f, 0.f, 0.f));
 	cam._cameraRotationMat = glm::rotate(cam._cameraRotationMat, rota[0], glm::vec3(0.f, 1.f, 0.f));
-
-	cam._modelView *= cam._cameraRotationMat;
 
 	cam._MVP = cam._projection * cam._modelView;
 
